3a849d9ba8b22c453447dc0958735e2cbee99cfa
Mateusz <Mateusz@LAPTOP-VP07MI1H>
2019-10-23 19:47:41 +0200

    register new beans with controllers and property file

--- project1/src/main/java/com/mk/multiscalemodeling/project1/Project1Configuration.java
+++ project1/src/main/java/com/mk/multiscalemodeling/project1/Project1Configuration.java
@@ -1,24 +1,56 @@
 package com.mk.multiscalemodeling.project1;
 
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
+import com.mk.multiscalemodeling.project1.controllers.SimulationController;
+import com.mk.multiscalemodeling.project1.controllers.SimulationParametersController;
+import com.mk.multiscalemodeling.project1.controllers.StartController;
 import com.mk.multiscalemodeling.project1.model.GrainsManager;
 import com.mk.multiscalemodeling.project1.simulation.SimulationManager;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootConfiguration
+@Configuration
 @ComponentScan({"com.mk.multiscalemodeling.project1.model", "com.mk.multiscalemodeling.project1.simulation"})
+@PropertySource(value = { "application.properties" })
 public class Project1Configuration {
 
 	@Bean
 	public GrainsManager getGrainsManager() {
+	    log.info("Initializing Grains Manager");
 		return new GrainsManager();
 	}
 
 	@Bean
 	public SimulationManager getSimulationManager() {
+	    log.info("Initializing Simulation Manager");
 	    return new SimulationManager();
+	}
+	
+	@Bean
+	public StartController getStartController() {
+	    log.info("Initializing: StartController");
+	    return new StartController();
+	}
+	
+	@Bean
+	public SimulationController getSimulationController() {
+	    log.info("Initializing: SimulationController");
+	    return new SimulationController();
+	}
+	
+	@Bean
+	public SimulationParametersController getSimulationParametersController() {
+	    log.info("Initializing: SimulationParametersController");
+	    return new SimulationParametersController();
 	}
 	
 }
