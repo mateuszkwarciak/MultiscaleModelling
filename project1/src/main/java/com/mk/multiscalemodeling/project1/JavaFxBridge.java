00cdd404302e48c9d6979372d641c038139ba783
Mateusz <Mateusz@LAPTOP-VP07MI1H>
2019-11-03 19:57:50 +0100

    fix errors with connection between spring and javaFx

--- project1/src/main/java/com/mk/multiscalemodeling/project1/JavaFxBridge.java
+++ project1/src/main/java/com/mk/multiscalemodeling/project1/JavaFxBridge.java
@@ -3,6 +3,8 @@ package com.mk.multiscalemodeling.project1;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.mk.multiscalemodeling.project1.controllers.StartController;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -16,6 +18,8 @@ public class JavaFxBridge extends Application {
 	private Parent rootNode;
     private FXMLLoader fxmlLoader;
     
+    private StartController startController;
+    
     public static final String APPLICATION_TITLE = "Kryszta³ki";
     
 	public static void main(String[] args) {
@@ -26,15 +30,19 @@ public class JavaFxBridge extends Application {
 	public void init() throws Exception {
     	fxmlLoader = new FXMLLoader();
     	fxmlLoader.setControllerFactory(clazz -> applicationContext.getBean(clazz));
+    	
 		super.init();
 	}
 
 	@Override
     public void start(Stage primaryStage) throws Exception {
     	fxmlLoader.setLocation(getClass().getResource("/fxml/start.fxml"));
-        rootNode = fxmlLoader.load();
-        
+    	fxmlLoader.setController(startController);
+
+    	rootNode = fxmlLoader.load();
+
         primaryStage.setTitle(APPLICATION_TITLE);
+        primaryStage.setResizable(false);
         
         Scene scene = new Scene(rootNode, 700, 400);
         primaryStage.setScene(scene);
